7f034e3a16d3b1f8eb7aac958e874009fa52d686
Benny Kritzer <benny.kritzer@gmail.com>
2021-04-05 19:42:03 -0400

    End of Section #4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectedEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleOpenCreateForm() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
 
   return (
     <>
-      <NavBar setFormOpen={setFormOpen}/>
+      <NavBar setFormOpen={handleOpenCreateForm} />
       <Container className='main'>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectEvent={handleSelectedEvent}
+          selectedEvent={selectedEvent}
+        />
       </Container>
     </>
   );
