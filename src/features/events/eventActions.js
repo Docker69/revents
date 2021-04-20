4285666c7cbba3177030845b5113cb9c6896692b
Benny Kritzer <benny.kritzer@gmail.com>
2021-04-19 23:06:11 -0400

    End of Section #19

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -6,6 +6,9 @@ import {
   LISTEN_TO_EVENT_CHAT,
   LISTEN_TO_SELECTED_EVENT,
   UPDATE_EVENT,
+  SET_FILTER,
+  SET_START_DATE,
+  CLEAR_SELECTED_EVENT,
 } from "./eventConstants";
 import {
   asyncActionError,
@@ -40,21 +43,21 @@ export function deleteEvent(eventId) {
   };
 }
 
-export function fetchEvents(predicate, limit, lastDocSnapshot) {
+export function fetchEvents(filter, startDate, limit, lastDocSnapshot) {
   return async function (dispatch) {
     dispatch(asyncActionStart());
     try {
       const snapshot = await fetchEventsFromFirestore(
-        predicate,
+        filter,
+        startDate,
         limit,
         lastDocSnapshot
-      ).get();
+     ).get();
       const lastVisible = snapshot.docs[snapshot.docs.length - 1];
       const moreEvents = snapshot.docs.length >= limit;
       const events = snapshot.docs.map((doc) => dataFromSnapshot(doc));
-      dispatch({ type: FETCH_EVENTS, payload: { events, moreEvents } });
+      dispatch({ type: FETCH_EVENTS, payload: { events, moreEvents, lastVisible } });
       dispatch(asyncActionFinish());
-      return lastVisible;
     } catch (error) {
       dispatch(asyncActionError(error));
       toast.error(error);
@@ -62,11 +65,31 @@ export function fetchEvents(predicate, limit, lastDocSnapshot) {
   };
 }
 
+export function setFilter(value) {
+  return function(dispatch) {
+    dispatch(clearEvents());
+    dispatch({type: SET_FILTER, payload: value})
+  }
+}
+
+export function setStartDate(date) {
+  return function(dispatch) {
+    dispatch(clearEvents());
+    dispatch({type: SET_START_DATE, payload: date})
+  }
+}
+
 export function listenToSelectedEvent(event) {
   return {
     type: LISTEN_TO_SELECTED_EVENT,
     payload: event,
   };
+}
+
+export function clearSelectedEvent() {
+  return {
+    type: CLEAR_SELECTED_EVENT
+  }
 }
 
 export function clearEvents() {
--- src/features/events/eventReducer.js
+++ src/features/events/eventReducer.js
@@ -1,11 +1,15 @@
 import {
   CLEAR_COMMENTS,
   CLEAR_EVENTS,
+  CLEAR_SELECTED_EVENT,
   CREATE_EVENT,
   DELETE_EVENT,
   FETCH_EVENTS,
   LISTEN_TO_EVENT_CHAT,
   LISTEN_TO_SELECTED_EVENT,
+  RETAIN_STATE,
+  SET_FILTER,
+  SET_START_DATE,
   UPDATE_EVENT,
 } from "./eventConstants";
 
@@ -14,6 +18,10 @@ const initialState = {
   comments: [],
   moreEvents: true,
   selectedEvent: null,
+  lastVisible: null,
+  filter: 'all',
+  startDate: new Date(),
+  retainState: false,
 };
 
 export default function eventReducer(state = initialState, { type, payload }) {
@@ -47,16 +55,22 @@ export default function eventReducer(state = initialState, { type, payload }) {
         ...state,
         events: [...state.events.filter((evt) => evt.id !== payload)],
       };
-    case LISTEN_TO_SELECTED_EVENT:
-      return {
-        ...state,
-        selectedEvent: payload,
-      };
-    case FETCH_EVENTS:
+      case LISTEN_TO_SELECTED_EVENT:
+        return {
+          ...state,
+          selectedEvent: payload,
+        };
+      case CLEAR_SELECTED_EVENT:
+        return {
+          ...state,
+          selectedEvent: null,
+        };
+      case FETCH_EVENTS:
       return {
         ...state,
         events: [...state.events, ...payload.events],
         moreEvents: payload.moreEvents,
+        lastVisible: payload.lastVisible,
       };
     case LISTEN_TO_EVENT_CHAT:
       return {
@@ -68,13 +82,33 @@ export default function eventReducer(state = initialState, { type, payload }) {
         ...state,
         comments: [],
       };
-    case CLEAR_EVENTS:
-      return {
-        ...state,
-        events: [],
-        moreEvents: true,
-      };
-    default:
+      case CLEAR_EVENTS:
+        return {
+          ...state,
+          events: [],
+          moreEvents: true,
+          lastVisible: null,
+        };
+      case SET_FILTER:
+        return {
+          ...state,
+          retainState: false,
+          moreEvents: true,
+          filter: payload,
+        };
+      case SET_START_DATE:
+        return {
+          ...state,
+          retainState: false,
+          moreEvents: true,
+          startDate: payload,
+        };
+      case RETAIN_STATE:
+        return {
+          ...state,
+          retainState: true,
+        };
+      default:
       return state;
   }
 }
